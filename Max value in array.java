a55a99a2d781cfa737c95bd3d791a60c67480723
PSBB SAI NIHARIKA <psainiharika2005@gmail.com>
2025-12-16 16:33:20 +0530

    Create Max value in array.java

---
+++ Max value in array.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        int[] arr = {10, 25, 5, 40, 15};
+        int max = arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        System.out.println("Maximum value is: " + max);
+    }
+}
